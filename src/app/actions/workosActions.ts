a9639c86e073ecbf962f15e7a206d74f6e177b8e
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-11 23:53:31 +0530

    workos actions to add new company

---
+++ src/app/actions/workosActions.ts
@@ -0,0 +1,11 @@
+"use server"
+
+import { WorkOS } from "@workos-inc/node";
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+export async function createCompany(data:FormData) {
+    "use server";
+    await workos.organizations.createOrganization({
+        name: data.get("newCompanyName") as string
+    })
+}
