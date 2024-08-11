7fe12efa5fb8c2c5ba7456f89fe74576d0fe2a1f
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-11 23:48:05 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,4 @@ import { authkitMiddleware } from '@workos-inc/authkit-nextjs';
 
 export default authkitMiddleware();
 
-// Match against pages that require authentication
-// Leave this out if you want authentication on every page in your application
-export const config = { matcher: ['/'] };
+export const config = { matcher: ['/','/new-listing'] };
