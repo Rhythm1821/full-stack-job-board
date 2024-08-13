269ad0b51b74f0fbe891036cfb1d89752dca565f
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-13 18:29:15 +0530

    ui for adding companies

--- src/app/actions/workosActions.ts
+++ src/app/actions/workosActions.ts
@@ -1,10 +1,14 @@
 "use server"
 
 import { WorkOS } from "@workos-inc/node";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY);
 export async function createCompany(companyName:string,userId:string) {
     "use server";
     const org = await workos.organizations.createOrganization({name: companyName})
-    return await workos.userManagement.createOrganizationMembership({userId, organizationId: org.id, roleSlug: "admin"})
+    await workos.userManagement.createOrganizationMembership({userId, organizationId: org.id, roleSlug: "admin"})
+    revalidatePath('/new-listing')
+    redirect('/new-listing')
 }
--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -26,7 +26,7 @@ export default async function NewListingPage() {
     const organizationMemberships = await workos.userManagement.listOrganizationMemberships({ userId: user.id })
 
     const activeOrganizationMemberships = organizationMemberships.data.filter(om => om.status === 'active')
-    const organizationsNames:{[key:string]:string} = {}
+    const organizationsNames: { [key: string]: string } = {}
     for (const activeMembership of activeOrganizationMemberships) {
         const organization = await workos.organizations.getOrganization(activeMembership.organizationId)
         organizationsNames[organization.id] = organization.name
@@ -37,12 +37,22 @@ export default async function NewListingPage() {
             {JSON.stringify(organizationsNames)}
             <div>
                 <h2 className="text-lg mt-4">Your companies</h2>
-                <p className="text-gray-500 text-sm mb-2">Select a company</p>
-                {organizationMemberships.data.filter(om => om.status === 'active').map(om => (
-                    <div>
-                        {om.organizationId}
+                <p className="text-gray-500 text-sm mb-2">Select a company to create a job add for</p>
+                <div>
+                    <div className="border rounded-md inline-block">
+                        {Object.keys(organizationsNames).map(orgId => (
+                            <Link
+                                className={
+                                    "py-2 px-4 flex gap-2 items-center "
+                                    + (Object.keys(organizationsNames)[0] === orgId ? '' : 'border-t')
+                                }
+                                href={`/new-listing/${orgId}`}>
+                                {organizationsNames[orgId]}
+                                <FontAwesomeIcon className="h-4" icon={faArrowRight} />
+                            </Link>
+                        ))}
                     </div>
-                ))}
+                </div>
 
                 {organizationMemberships.data.length === 0 && (
                     <div className="border border-blue-200 bg-blue-50 p-4 rounded-md">
