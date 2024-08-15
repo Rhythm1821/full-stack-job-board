759d12caca71dd7288c272f5d08e38238ca25bb5
Rhythm1821 <alamrawat1821@gmail.com>
2024-08-15 23:45:06 +0530

    Integrating form to the ui

--- src/app/new-listing/[orgId]/page.tsx
+++ src/app/new-listing/[orgId]/page.tsx
@@ -1,30 +1,27 @@
-import { getUser } from "@workos-inc/authkit-nextjs"
-import { WorkOS } from "@workos-inc/node"
+import JobForm from "@/app/components/JobForm";
+import {getUser} from "@workos-inc/authkit-nextjs";
+import {WorkOS} from "@workos-inc/node";
 
 type PageProps = {
-    params: {
-        orgId: string
-    }
-}
+  params: {
+    orgId: string;
+  }
+};
 
-export default async function NewListingOrgPage(props:PageProps){
-    const { user } = await getUser()
-    const workos = new WorkOS(process.env.WORKOS_API_KEY)
-    if (!user) return <div>You must be signed in to post a job</div>
-    const orgId = props.params.orgId
-    const oms = await workos.userManagement.listOrganizationMemberships({ userId: user.id})
-    const om = oms.data.find(om=>om.organizationId===orgId)
-    const hasAccess = oms.data.length>0 
-    
-    if (!hasAccess) {
-        return (
-            <div>You do not have access to this organization</div>
-        )
-    }
+export default async function NewListingForOrgPage(props:PageProps) {
+  const {user} = await getUser();
+  const workos = new WorkOS(process.env.WORKOS_API_KEY);
+  if (!user) {
+    return 'Please log in';
+  }
+  const orgId = props.params.orgId;
+  const oms = await workos.userManagement.listOrganizationMemberships({userId:user.id,organizationId:orgId});
+  const hasAccess = oms.data.length > 0;
+  if (!hasAccess) {
+    return 'no access'
+  }
 
-    return (
-        <form className="container mt-6" action="">
-            <input type="text" placeholder="job title" className="border p-2" />
-        </form>
-    )
+  return (
+    <JobForm orgId={orgId} />
+  );
 }
